63f87fc6f42c4b10306dcaa007a0024fce5fe631
rahibamin <54030063+rahibamin@users.noreply.github.com>
2019-08-12 13:39:26 -0400

    Add files via upload

---
+++ SumOfTwoNumbers.java
@@ -0,0 +1,17 @@
+// Author Rahib Amin
+// Session 1 Assignment 2
+
+
+public class SumOfTwoNumbers {
+
+	public static void main(String[] args) {
+		
+		int x = 5;
+		int y = -10;
+		int z = (x -+ y);
+		
+		System.out.println(z);
+
+	}
+
+}
